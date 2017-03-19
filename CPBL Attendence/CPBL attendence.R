6ba866452e2d590fb0326e0aa63743039d74c47b
andytsai8 <r04524043@ntu.edu.tw>
2017-03-19 14:05:07 +0800

    CPBL

--- CPBL Attendence/CPBL attendence.R
+++ CPBL Attendence/CPBL attendence.R
@@ -9,6 +9,6 @@ ani.options("convert")
 saveGIF(
   for(yid in c(1:length(rawData[,1])))
   {
-    barplot(as.matrix(rawData[yid,c(-1,-2)]), main = paste0("中華職棒 ", rawData[yid,1]," 年進場人數總計 ",rawData[yid,2]," 人"))
+    barplot(as.matrix(rawData[yid,c(-1,-2)]), main = paste0("中華職棒 ", rawData[yid,1]," 年進場人數總計 ",rawData[yid,2]," 人"), ylim=c(0,8000))
   },movie.name = "CPBL attendence.gif",
-  interval = 0.5, ani.width = 1000, ani.height = 500)
+  interval = 0.8, ani.width = 800, ani.height = 500)
